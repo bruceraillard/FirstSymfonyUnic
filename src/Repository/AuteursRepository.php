41f65914238108c80f543efe62dcfb31cd989a51
Raillard Bruce <bruce.raillard@etudiant.unc.nc>
2025-03-01 17:04:58 +1100

    Ajout d'un requete pour lister les auteurs ayant ecrit plus d'un livre

--- src/Repository/AuteursRepository.php
+++ src/Repository/AuteursRepository.php
@@ -16,6 +16,48 @@ class AuteursRepository extends ServiceEntityRepository
         parent::__construct($registry, Auteurs::class);
     }
 
+    // query to find authors who have written more than one book
+    public function findMultiBookAuthor()
+    {
+        return $this->getEntityManager()
+            ->createQuery('
+            SELECT a
+            FROM App\Entity\Auteurs a
+            JOIN a.livres l
+            GROUP BY a.id
+            HAVING COUNT(l) > 1
+        ')
+            ->getResult();
+    }
+
+    //    /**
+    //     * @return Auteurs[] Returns an array of Auteurs objects
+    //     */
+    /*
+    public function findByExampleField($value): array
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('a.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    //    public function findOneBySomeField($value): ?Auteurs
+    //    {
+    //        return $this->createQueryBuilder('a')
+    //            ->andWhere('a.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+
+
     //    /**
     //     * @return Auteurs[] Returns an array of Auteurs objects
     //     */
