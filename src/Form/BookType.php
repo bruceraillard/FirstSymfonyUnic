9a1699e85f4c7ddef03894ce16085594eae79c61
Raillard Bruce <bruce.raillard@etudiant.unc.nc>
2025-03-27 11:04:49 +1100

    Création de BookType pour créer un formulaire avec la commande 'php bin/console make:form. BokkType est liée à l'entité 'Livres'

---
+++ src/Form/BookType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Auteurs;
+use App\Entity\Livres;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('publicationdate', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('publishinghouse')
+            ->add('auteur', EntityType::class, [
+                'class' => Auteurs::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livres::class,
+        ]);
+    }
+}
